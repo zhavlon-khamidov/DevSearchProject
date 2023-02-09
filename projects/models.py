ebc35ae526bb619fa7d93879f88663ef9afcc3b6
Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2023-02-09 09:10:50 +0600

    First Model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+
+
+    def __str__(self):
+        return self.title
+    
