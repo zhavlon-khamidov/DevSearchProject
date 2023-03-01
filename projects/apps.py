b018d7e8d22c20b50ea7c06a831c793c5e4306b3
Zhavlon <zhavlon.khamidov@alatoo.edu.kg>
2023-03-01 09:50:40 +0600

    intro to signals

--- projects/apps.py
+++ projects/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from django.core.signals import request_finished
 
+
+def my_callback(sender, **kwargs):
+    print("request finished")
 
 class ProjectsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'projects'
+
+    def ready(self):
+        request_finished.connect(my_callback)
